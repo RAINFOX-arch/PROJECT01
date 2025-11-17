5b0f5dc328c26426c0df49d1dccddb0234cb9cfb
RAINFOX-arch <xin7996@gmail.com>
2025-11-17 05:44:58 +0000

    update code

--- fact_collector/core/scheduler.py
+++ fact_collector/core/scheduler.py
@@ -3,7 +3,7 @@ import time
 import subprocess
 def run_collector():
     subprocess.run(["python","fact_collector/main.py"])
-    schedule.every().day.at("08:00").do(run_collector)
+    schedule.every().day.at("00:00").do(run_collector)
     print("自動排程啟動中...（按 Ctrl + C 結束）")
 while True:
     schedule.run_pending()
